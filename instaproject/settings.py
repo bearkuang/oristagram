172fbb131828958c259469c293cf48f1533577d7
SeungJu <137677440+bearkuang@users.noreply.github.com>
2024-07-14 14:23:01 +0900

    [Update] settings.py

--- instaproject/settings.py
+++ instaproject/settings.py
@@ -106,12 +106,15 @@ AUTH_USER_MODEL = 'instaapp.CustomUser'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'instagram',
-        'USER': 'ori',
-        'PASSWORD': 'oripassword',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    },
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
 }
 
 # Password validation
